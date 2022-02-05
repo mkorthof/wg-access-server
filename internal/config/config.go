6c2634ca8c25ed57c6580b6a5f4960d854ae69ea
Marius <m@mari.us.com>
2022-02-05 21:56:23 +0100

    add bind address option

--- internal/config/config.go
+++ internal/config/config.go
@@ -23,7 +23,7 @@ type AppConfig struct {
 	AdminUsername string `yaml:"adminUsername"`
 	// Set the superadmin password (required)
 	AdminPassword string `yaml:"adminPassword"`
-	// Addr sets the address (host/ip) that the web UI will listen on.
+	// Addr sets the address (ip/port) that the web UI will listen on.
 	// Defaults to 0.0.0.0
 	Address string `yaml:"addr"`
 	// Port sets the port that the web UI will listen on.
